a0d30a6825fcd0783d793e02921a22ec2a690f71
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-07-19 23:38:02 +0300

    Create string.py
    Пробую разные свойства и фукции

---
+++ string.py
@@ -0,0 +1,18 @@
+# Пробуем строки
+
+string_x = 'Привет всем от "Чайки"'
+string_x2 = "Hello all, \"Chayka\""
+print(string_x)
+print(string_x2)
+
+string_x3 = string_x + string_x2
+print(string_x3)
+
+name = 'Go '
+print(name * 20)
+
+my_string = 'Python'
+for mtg in dir(my_string):
+    print(mtg)
+    
+print('#' * 28)
