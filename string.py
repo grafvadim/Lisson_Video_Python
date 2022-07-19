8ae2c325a87c8c5928dc5184d0a77ce5fc36d1ee
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-07-20 00:31:00 +0300

    Update string.py
    дальше осваиваем разные фукции

--- string.py
+++ string.py
@@ -16,3 +16,38 @@ for mtg in dir(my_string):
     print(mtg)
     
 print('#' * 28)
+
+my_string = my_string.upper()
+print(my_string)
+print('-' * 30)
+my_string = my_string.lower()
+print(my_string)
+print('-' * 30)
+my_string = my_string.title()
+print(my_string)
+print('-' * 30)
+
+my_string = 'My city Doneck'
+print(my_string)
+my_string = my_string.replace('Doneck', 'Krefild')
+print(my_string)
+
+print('-' * 30)
+
+my_string = 'My city Doneck'
+print(my_string)
+my_string = my_string.split(' ')
+print(my_string)
+print(f'Тип my_string: {type(my_string)}')
+
+my_string = ' '.join(my_string)
+print(my_string)
+
+my_string = ' I like Python'
+my_string = my_string.strip()
+print(my_string)
+
+name = 'Федор'
+name_1 = 'Анна'
+my_string = f'Привет {name}, {name_1}'
+print(my_string)
