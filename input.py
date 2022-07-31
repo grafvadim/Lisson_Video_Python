3e85c2963fd110baa4050c04d265d7a8fb9c4a41
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-07-31 22:50:27 +0300

    Create input.py
    изучаю input

---
+++ input.py
@@ -0,0 +1,12 @@
+# Площадь прямоугольника
+share_len = float(input('Введите длину прямоугольника: '))
+share_wirth = float(input('Введите ширину прямоугольника: '))
+share_area = share_len * share_wirth
+print(f'Площадь прямоугольника: {share_area}')
+
+# Ваш возраст
+age = int(input('Введите свой возраст: '))
+if age < 18:
+    print('Проходи мимо, сынок!')
+else:
+    print('Добро пожаловать на наш Хаб!')
