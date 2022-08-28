230b3ff43a6977d91354bbd1ea749a538492c9f2
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-08-28 13:32:47 +0300

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,14 @@
+my_dict = {}
+my_dict2 = dict()
+print(type(my_dict))
+print(type(my_dict2))
+
+my_dict = {
+    "имя": "Виктор", "фамилия": "Петров", "возраст": 45, "цвет глаз": "карие"   
+}
+print(my_dict["фамилия"])
+
+my_dict["вес"] = 86
+my_dict["любимое блюдо"] = "лапша"
+
+print(my_dict)
